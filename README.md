b9551a9ccb10c4d1a5ea653f653b0bbfc7a1339d
jaywcjlove <398188662@qq.com>
2021-01-06 00:09:36 +0800

    feat: Add test input feild.

--- README.md
+++ README.md
@@ -4,3 +4,7 @@ create-tag-action
 [![build-test](https://github.com/jaywcjlove/create-tag-action/workflows/build-test/badge.svg)](https://github.com/actions/typescript-action/actions)
 
 Auto create tags from commit.
+
+## Related
+
+- [Github Release Changelog Generator](https://github.com/jaywcjlove/changelog-generator) A GitHub Action that compares the commit differences between two branches
--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,55 @@
 import * as path from 'path'
+import * as semver from 'semver'
 import * as core from '@actions/core'
 import * as github from '@actions/github'
+
+const getVersion = (ver: string): string => {
+  let currentVersion = ''
+  ver.replace(/([v|V]\d(\.\d+){0,2})/i, (str: string) => {
+    currentVersion = str
+    return str
+  })
+  return currentVersion
+}
 
 async function run(): Promise<void> {
   try {
     const myToken = core.getInput('token')
+    const test = core.getInput('test')
     const packagePath = core.getInput('package-path')
     const octokit = github.getOctokit(myToken)
     const {owner, repo} = github.context.repo
-    const commit = github.context.payload.head_commit.message
+    const commit: string = github.context.payload.head_commit.message
     const listTags = await octokit.repos.listTags({
       owner: (owner as unknown) as string,
       repo: (repo as unknown) as string
     })
-    console.log('commit>', commit)
-    console.log('commit>', listTags.data)
-
-    if (packagePath) {
-      const resolvePackagePath = path.resolve(__dirname, packagePath)
-      console.log('commit>', resolvePackagePath)
+    if (!test && !packagePath) {
+      return
     }
+    /** current version, example: `v1.0.1` */
+    let version = ''
+
+    if ((test && !new RegExp(test).test(commit)) || (!test && !packagePath)) {
+      return
+    }
+    if (test && new RegExp(test).test(commit)) {
+      version = getVersion(commit)
+      if (!version) return
+      if (
+        listTags.data[0] &&
+        !semver.gt(version, (listTags.data[0] as unknown) as string)
+      ) {
+        return
+      }
+    } else {
+      const resolvePackagePath = path.resolve(__dirname, packagePath)
+      console.log('Resolve Package Path >>>', resolvePackagePath)
+    }
+    core.info(`Tag: ${version}`)
+    console.log('commit >>>', commit)
+    console.log('List Tags >>>', listTags.data)
+    console.log('version>', listTags.data)
   } catch (error) {
     core.setFailed(error.message)
   }
