90beda4a2051235305cf0584fd45c27a90788b1f
jaywcjlove <jaywcjlove@users.noreply.github.com>
2026-04-21 09:18:23 +0000

    v2.3.0 doc: Update README a9b54d7b5e0cd0670eccefc0afd56688adae1522

--- main.js
+++ main.js
@@ -11090,6 +11090,9 @@ function stringify(obj) {
     spaces = _ref.spaces;
   var EOF = finalEOL ? EOL : '';
   var str = JSON.stringify(obj, replacer, spaces);
+  if (str === undefined) {
+    throw new TypeError("Converting ".concat(typeof obj, " value to JSON is not supported"));
+  }
   return str.replace(/\n/g, EOL) + EOF;
 }
 function stripBom(content) {
